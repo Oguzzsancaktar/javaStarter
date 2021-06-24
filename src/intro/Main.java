4e3b8e9e0b0bf1bff043dc726c04be75448148d9
oguzzsancaktar <oguztahasancaktar@gmail.com>
2021-06-24 20:26:33 +0300

    If else maked !

--- src/intro/Main.java
+++ src/intro/Main.java
@@ -3,9 +3,24 @@ package intro;
 public class Main {
 
 	public static void main(String[] args) {
-		String testString = "Test String is this";
-		System.out.println(testString);
-		 
+		String myName = "Oğuz!";
+		String myWomanName = "Damla!";
+		double myLength = 1.83;
+		double myWomanLength = 1.60;
+		int myAge =  22;
+		boolean isAlive = true;
+		
+		System.out.println(isAlive);
+		
+		if (myLength > myWomanLength) {
+			System.out.println(myName);
+		}else if(myWomanLength == myLength) {
+			System.out.println("eşit");
+		}
+		else {
+			
+			System.out.println(myWomanName);
+		}
 	}
 
 }
